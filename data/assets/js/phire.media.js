79fbe9badf0526db395731980d84fcc4527d7880
Nick Sagona <nick@nolainteractive.com>
2016-06-20 14:20:05 -0500

    Update remote editor refs

--- data/assets/js/phire.media.js
+++ data/assets/js/phire.media.js
@@ -124,7 +124,9 @@ phire.loadEditor = function(editor, id) {
                 }
                 var eid = phire.editorIds[i].id;
                 jax('#field_' + eid).keyup(function(){
-                    CKEDITOR.instances['field_' + eid].setData(jax('#field_' + eid).val());
+                    if ((typeof CKEDITOR !== 'undefined') && (CKEDITOR.instances['field_' + eid] != undefined)) {
+                        CKEDITOR.instances['field_' + eid].setData(jax('#field_' + eid).val());
+                    }
                 });
             } else if (editor.indexOf('tinymce') != -1) {
                 if (tinymce.editors['field_' + phire.editorIds[i].id] == undefined) {
@@ -162,7 +164,9 @@ phire.loadEditor = function(editor, id) {
                 }
                 var eid = phire.editorIds[i].id;
                 jax('#field_' + eid).keyup(function(){
-                    tinymce.editors['field_' + eid].setContent(jax('#field_' + eid).val());
+                    if ((typeof tinymce !== 'undefined') && (tinymce.editors['field_' + eid] != undefined)) {
+                        tinymce.editors['field_' + eid].setContent(jax('#field_' + eid).val());
+                    }
                 });
             }
         }
